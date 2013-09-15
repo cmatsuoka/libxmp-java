083bdad04795f17757219da14df46ca547cfd4c8
Claudio Matsuoka <cmatsuoka@gmail.com>
2013-09-14 23:23:31 -0300

    Add frame info fields
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- jni/interface.c
+++ jni/interface.c
@@ -38,28 +38,22 @@ METHOD(jboolean, xmpTestModule) (JNIEnv *env, jobject obj, jstring path, jobject
 	(*env)->ReleaseStringUTFChars(env, path, filename);
 
 	if (res == 0) {
-		if (info != NULL) {
-			jclass modInfoClass = (*env)->FindClass(env,
+		jclass modInfoClass = (*env)->FindClass(env,
 	                        	"org/helllabs/java/libxmp/TestInfo");
-			jfieldID field;
+		jfieldID field;
 	
-			if (modInfoClass == NULL)
-				return JNI_FALSE;
+		if (modInfoClass == NULL)
+			return JNI_FALSE;
 			
-			field = (*env)->GetFieldID(env, modInfoClass, "name",
-	                        	"Ljava/lang/String;");
-			if (field == NULL)
-				return JNI_FALSE;
-			(*env)->SetObjectField(env, info, field,
+		field = (*env)->GetFieldID(env, modInfoClass, "name",
+					"Ljava/lang/String;");
+		(*env)->SetObjectField(env, info, field,
 					(*env)->NewStringUTF(env, ti.name));
-	
-			field = (*env)->GetFieldID(env, modInfoClass, "type",
-	                        	"Ljava/lang/String;");
-			if (field == NULL)
-				return JNI_FALSE;
-			(*env)->SetObjectField(env, info, field,
+
+		field = (*env)->GetFieldID(env, modInfoClass, "type",
+					"Ljava/lang/String;");
+		(*env)->SetObjectField(env, info, field,
 					(*env)->NewStringUTF(env, ti.type));
-		}
 
 		return JNI_TRUE;
 	}
@@ -75,6 +69,74 @@ METHOD(void, xmpReleaseModule) (JNIEnv *env, jobject obj, jlong ctx)
 METHOD(jint, xmpStartPlayer) (JNIEnv *env, jobject obj, jlong ctx, jint rate, jint flags)
 {
 	return xmp_start_player((xmp_context)ctx, rate, flags);
+}
+
+METHOD(jobject, xmpGetFrameInfo) (JNIEnv *env, jobject obj, jlong ctx, jobject info)
+{
+	struct xmp_frame_info fi;
+	jclass modInfoClass;
+	jfieldID field;
+
+	xmp_get_frame_info((xmp_context)ctx, &fi);
+
+	modInfoClass = (*env)->FindClass(env,
+                       	"org/helllabs/java/libxmp/FrameInfo");
+
+	if (modInfoClass == NULL)
+		return JNI_FALSE;
+	
+	field = (*env)->GetFieldID(env, modInfoClass, "pos", "I");
+	(*env)->SetIntField(env, info, field, fi.pos);
+
+	field = (*env)->GetFieldID(env, modInfoClass, "pattern", "I");
+	(*env)->SetIntField(env, info, field, fi.pattern);
+
+	field = (*env)->GetFieldID(env, modInfoClass, "row", "I");
+	(*env)->SetIntField(env, info, field, fi.row);
+
+	field = (*env)->GetFieldID(env, modInfoClass, "numRows", "I");
+	(*env)->SetIntField(env, info, field, fi.num_rows);
+
+	field = (*env)->GetFieldID(env, modInfoClass, "frame", "I");
+	(*env)->SetIntField(env, info, field, fi.frame);
+
+	field = (*env)->GetFieldID(env, modInfoClass, "speed", "I");
+	(*env)->SetIntField(env, info, field, fi.speed);
+
+	field = (*env)->GetFieldID(env, modInfoClass, "bpm", "I");
+	(*env)->SetIntField(env, info, field, fi.bpm);
+
+	field = (*env)->GetFieldID(env, modInfoClass, "time", "I");
+	(*env)->SetIntField(env, info, field, fi.time);
+
+	field = (*env)->GetFieldID(env, modInfoClass, "totalTime", "I");
+	(*env)->SetIntField(env, info, field, fi.total_time);
+
+	field = (*env)->GetFieldID(env, modInfoClass, "frameTime", "I");
+	(*env)->SetIntField(env, info, field, fi.frame_time);
+
+	field = (*env)->GetFieldID(env, modInfoClass, "bufferSize", "I");
+	(*env)->SetIntField(env, info, field, fi.buffer_size);
+
+	field = (*env)->GetFieldID(env, modInfoClass, "totalSize", "I");
+	(*env)->SetIntField(env, info, field, fi.total_size);
+
+	field = (*env)->GetFieldID(env, modInfoClass, "volume", "I");
+	(*env)->SetIntField(env, info, field, fi.volume);
+
+	field = (*env)->GetFieldID(env, modInfoClass, "loopCount", "I");
+	(*env)->SetIntField(env, info, field, fi.loop_count);
+
+	field = (*env)->GetFieldID(env, modInfoClass, "virtChannels", "I");
+	(*env)->SetIntField(env, info, field, fi.virt_channels);
+
+	field = (*env)->GetFieldID(env, modInfoClass, "virtUsed", "I");
+	(*env)->SetIntField(env, info, field, fi.virt_used);
+
+	field = (*env)->GetFieldID(env, modInfoClass, "sequence", "I");
+	(*env)->SetIntField(env, info, field, fi.sequence);
+
+	return info;
 }
 
 METHOD(void, xmpEndPlayer) (JNIEnv *env, jobject obj, jlong ctx)
--- src/org/helllabs/java/libxmp/FrameInfo.java
+++ src/org/helllabs/java/libxmp/FrameInfo.java
@@ -1,5 +1,21 @@
 package org.helllabs.java.libxmp;
 
 public class FrameInfo {
-
+	public int pos;				/* Current position */
+	public int pattern;			/* Current pattern */
+	public int row;				/* Current row in pattern */
+	public int numRows;			/* Number of rows in current pattern */
+	public int frame;			/* Current frame */
+	public int speed;			/* Current replay speed */
+	public int bpm;				/* Current bpm */
+	public int time;			/* Current module time in ms */
+	public int totalTime;		/* Estimated replay time in ms*/
+	public int frameTime;		/* Frame replay time in us */
+	public int bufferSize;		/* Used buffer size */
+	public int totalSize;		/* Total buffer size */
+	public int volume;			/* Current master volume */
+	public int loopCount;		/* Loop counter */
+	public int virtChannels;	/* Number of virtual channels */
+	public int virtUsed;		/* Used virtual channels */
+	public int sequence;		/* Current sequence */
 }
