491a2e2f77d3149077fa6741ab266ff644b9776b
Claudio Matsuoka <cmatsuoka@gmail.com>
2014-03-12 14:01:41 -0300

    Update showpattern example
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- examples/showpattern/src/com/example/ShowPattern.java
+++ examples/showpattern/src/com/example/ShowPattern.java
@@ -4,7 +4,7 @@
  * This code is in the public domain.
  */
 
-package com.example.showpattern;
+package com.example;
 
 import java.io.IOException;
 
@@ -22,49 +22,46 @@ public class ShowPattern {
 		System.out.println("Module length: " + module.getLength() + " patterns");
 	}
 	
-	private String formatNote(int n) {
+	private String formatNote(final int num) {
 		final String[] note = {
 			"C ", "C#", "D ", "D#", "E ", "F ",
 			"F#", "G ", "G#", "A ", "A#", "B "
 		};
-		if (n++ == 0)
+		
+		if (num == 0) {
 			return "---";
-		else
-			return String.format("%s%d", note[n % 12], n / 12);
+		} else {
+			return String.format("%s%d", note[(num - 1) % 12], (num - 1) / 12);
+		}
 	}
 	
-	private String formatNum(int n) {
-		if (n == 0)
-			return "--";
-		else
-			return String.format("%02X", n);
+	private String formatNum(final int num) {
+		return num == 0 ? "--" : String.format("%02X", num);
 	}
 	
-	private void showEvent(Event e) {
+	private void showEvent(final Event e) {
 		System.out.printf(" | %s %s %s", formatNote(e.note), formatNum(e.ins), formatNum(e.vol));
 	}
 
-	private void run(String path, int num) throws IOException {
-		Player player = new Player();
-		Module module = player.loadModule(path);
+	private void run(final String path, final int num) throws IOException {
+		final Player player = new Player();
+		final Module module = player.loadModule(path);
 		
 		showHeader(module);
 		
-		Pattern pattern = module.getPattern(num);
-		Event event = new Event();
+		final Pattern pattern = module.patternData(num);
+		final Event event = new Event();
 		
 		System.out.println("\nPATTERN " + num);
 		
-		for (int row = 0; row < pattern.getRows(); row++) {
+		for (int row = 0; row < pattern.getNumRows(); row++) {
 			System.out.printf("%02X", row);
-			for (int chn = 0; chn < pattern.getChannels(); chn++) {
-				pattern.getEvent(row, chn, event);
+			for (int chn = 0; chn < module.getNumChannels(); chn++) {
+				pattern.eventData(row, chn, event);
 				showEvent(event);
 			}
 			System.out.println();
 		}
-		
-
 	}
 
 	public static void main(String[] args) {
@@ -75,12 +72,12 @@ public class ShowPattern {
 			System.exit(0);
 		}
 
-		String path = args[0];
-		int num = Integer.parseInt(args[1]);
+		final String path = args[0];
+		final int num = Integer.parseInt(args[1]);
 
 		try {
 			Module.test(path);
-			ShowPattern app = new ShowPattern();
+			final ShowPattern app = new ShowPattern();
 			app.run(path, num);
 		} catch (IOException e) {
 			System.out.print("\nError: " + path + ": " + e.getMessage());
